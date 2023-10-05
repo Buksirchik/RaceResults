7940090e8a0db28d7eb2e1fb174d9a7b30a1a37e
Buksirchik <rusbuloichyk@gmail.com>
2023-10-05 21:39:20 +0300

    add Toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { RootNavigator } from './navigation';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export const App = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <RootNavigator />
+        <SafeAreaProvider>
+          <RootNavigator />
+        </SafeAreaProvider>
       </PersistGate>
     </Provider>
   );
--- src/navigation/RootNavigator/RootNavigator.tsx
+++ src/navigation/RootNavigator/RootNavigator.tsx
@@ -1,10 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { MainStackNavigator } from '../MainStackNavigator';
+import Toast from 'react-native-toast-message';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const RootNavigator = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
     <NavigationContainer>
       <MainStackNavigator />
+      <Toast topOffset={top} />
     </NavigationContainer>
   );
 };
