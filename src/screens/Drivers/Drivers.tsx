de94cc98b96383085f9c59c7e4da3981f90197b8
Buksirchik <rusbuloichyk@gmail.com>
2023-10-05 16:38:12 +0300

    add Drivers page

--- src/screens/Drivers/Drivers.tsx
+++ src/screens/Drivers/Drivers.tsx
@@ -1,5 +1,28 @@
-import { Text } from 'react-native';
+import { FlatList, FlatListProps, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { loadDrivers } from '../../store/drivers';
+import { getDriversSelector } from '../../store/drivers/selectors';
+import { Driver } from '../../types';
+
+const renderItem: FlatListProps<Driver>['renderItem'] = ({ item }) => {
+  const fullName = `${item.givenName} ${item.familyName}`;
+
+  return (
+    <View>
+      <Text>{fullName}</Text>
+    </View>
+  );
+};
 
 export const Drivers = () => {
-  return <Text>Drivers</Text>;
+  const dispatch = useAppDispatch();
+
+  const drivers = useAppSelector(getDriversSelector);
+
+  useEffect(() => {
+    dispatch(loadDrivers());
+  }, []);
+
+  return <FlatList data={drivers} renderItem={renderItem} />;
 };
