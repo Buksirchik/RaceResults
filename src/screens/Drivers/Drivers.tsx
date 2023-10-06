1070e42ddd3387e7484f55fe47040da866e8cc9c
Buksirchik <rusbuloichyk@gmail.com>
2023-10-06 09:41:41 +0300

    change Drivers screen

--- src/screens/Drivers/Drivers.tsx
+++ src/screens/Drivers/Drivers.tsx
@@ -1,11 +1,13 @@
 import { ActivityIndicator, FlatList, FlatListProps, SafeAreaView } from 'react-native';
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { loadDrivers } from '../../store/drivers';
+import { loadDrivers, refreshDrivers } from '../../store/drivers';
 import { driversStateSelector } from '../../store/drivers/selectors';
 import { Driver } from '../../types';
 import { styles } from './styles';
 import { DriverItem } from './DriverItem';
+import { EmptyView } from '../../components';
+import { DRIVER_ITEM_HEIGHT } from '../../constants';
 
 const renderItem: FlatListProps<Driver>['renderItem'] = ({ item, index }) => {
   return <DriverItem driver={item} order={index + 1} />;
@@ -13,20 +15,24 @@ const renderItem: FlatListProps<Driver>['renderItem'] = ({ item, index }) => {
 
 const keyExtractor: FlatListProps<Driver>['keyExtractor'] = item => item.driverId;
 
+const getItemLayout: FlatListProps<Driver>['getItemLayout'] = (data, index) => ({
+  length: DRIVER_ITEM_HEIGHT,
+  offset: DRIVER_ITEM_HEIGHT * index,
+  index,
+});
+
 export const Drivers = () => {
   const dispatch = useAppDispatch();
 
-  const { drivers, currentPage, totalPages, loading: isLoading } = useAppSelector(driversStateSelector);
+  const { drivers, refreshing, currentPage, totalPages, loading: isLoading } = useAppSelector(driversStateSelector);
 
   useEffect(() => {
-    if (!drivers.length) {
-      dispatch(loadDrivers());
-    }
-  }, [drivers.length]);
+    dispatch(loadDrivers(true));
+  }, []);
 
   const isInitialLoading = currentPage === 0;
 
-  const ListEmptyComponent = isLoading && isInitialLoading ? <ActivityIndicator size={'large'} /> : null;
+  const ListEmptyComponent = isLoading && isInitialLoading ? <ActivityIndicator size={'large'} /> : <EmptyView />;
   const ListFooterComponent = isLoading && !isInitialLoading ? <ActivityIndicator size={'large'} /> : null;
 
   const onEndReached = () => {
@@ -35,15 +41,24 @@ export const Drivers = () => {
     }
   };
 
+  const onRefresh = () => {
+    dispatch(refreshDrivers());
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data={drivers}
+        windowSize={11}
+        initialNumToRender={15}
+        getItemLayout={getItemLayout}
+        refreshing={refreshing}
+        onRefresh={onRefresh}
         keyExtractor={keyExtractor}
         renderItem={renderItem}
         ListEmptyComponent={ListEmptyComponent}
         contentContainerStyle={styles.listContentContainer}
-        onEndReachedThreshold={0.25}
+        onEndReachedThreshold={0.3}
         onEndReached={onEndReached}
         ListFooterComponent={ListFooterComponent}
       />
--- src/store/drivers/actions.ts
+++ src/store/drivers/actions.ts
@@ -2,11 +2,13 @@ import { Driver } from '../../types';
 
 export enum ActionType {
   GET_DRIVERS_LOADING = 'DRIVERS/GET_DRIVERS_LOADING',
+  GET_DRIVERS_REFRESHING = 'DRIVERS/GET_DRIVERS_REFRESHING',
   GET_DRIVERS_SUCCESS = 'DRIVERS/GET_DRIVERS_SUCCESS',
-  GET_DRIVERS_ERROR = 'DRIVERS/GET_DRIVERS_ERROR',
 }
 
 type GetDriversLoadingAction = { type: ActionType.GET_DRIVERS_LOADING };
+
+type GetDriversRefreshingAction = { type: ActionType.GET_DRIVERS_REFRESHING };
 
 type GetDriversSuccessAction = {
   type: ActionType.GET_DRIVERS_SUCCESS;
@@ -18,9 +20,14 @@ type GetDriversSuccessAction = {
 };
 
 export const getDriversLoadingAction = (): GetDriversLoadingAction => ({ type: ActionType.GET_DRIVERS_LOADING });
+
+export const getDriversRefreshingAction = (): GetDriversRefreshingAction => ({
+  type: ActionType.GET_DRIVERS_REFRESHING,
+});
+
 export const getDriversSuccessAction = (payload: GetDriversSuccessAction['payload']): GetDriversSuccessAction => ({
   type: ActionType.GET_DRIVERS_SUCCESS,
   payload,
 });
 
-export type DriversActions = GetDriversLoadingAction | GetDriversSuccessAction;
+export type DriversActions = GetDriversLoadingAction | GetDriversSuccessAction | GetDriversRefreshingAction;
